410da78e664a96bb40b88856727e4e870c3d20bb
procodingclass <88050205+procodingclass@users.noreply.github.com>
2021-11-15 14:42:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Mouse = Matter.Mouse;
+const Constraint = Matter.Constraint;
+
+var points = [
+  { x: 400, y: 200 },
+  { x: 600, y: 50 },
+  { x: 650, y: 300},
+  { x: 800, y: 180}
+];
+
+function preload(){
+  bgImg = loadImage("assets/background.png");
+  playerImg = loadImage("./assets/player1.png");
+}
+
+function setup() {
+  createCanvas(850, 400);
+  engine = Engine.create();
+  world = engine.world;
+
+  player = new Player(100, 200, 30, 30, playerImg);
+
+  sling = new Sling(points[0], player.body);
+  
+ 
+}
+
+function draw() {
+  background(bgImg);
+  Engine.update(engine);
+
+  sling.display();
+  player.display();
+
+  
+  for(var i=0; i< points.length; i++){
+    drawPoints(i);
+  }
+
+  
+}
+
+
+
+function drawPoints(i) {
+  push();
+  stroke("white")
+  strokeWeight(3);
+  ellipseMode(RADIUS);
+  fill("blue");
+  ellipse(points[i].x, points[i].y, 10, 10);
+  pop();
+}
+
+function mouseDragged(){
+  Body.setPosition(player.body, {x:mouseX, y:mouseY});
+}
